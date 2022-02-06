f18221d7eae0a3ab0e293b47ec97589459eb0333
Илья <xcaz@inbox.ru>
2022-02-06 16:04:34 +0400

    Fixes

--- src/test/java/ru/galyuk/test/processor/HtmlProcessorTest.java
+++ src/test/java/ru/galyuk/test/processor/HtmlProcessorTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class HtmlProcessorTest {
@@ -12,7 +14,7 @@ public class HtmlProcessorTest {
     @Test
     public void processHtmlTest() {
         HtmlProcessor htmlProcessor = new HtmlProcessor();
-        Map<String, Integer> result = htmlProcessor.processHtml(getHtmlFromResource("src/test/resources/htmlTest.html"));
+        Map<String, Integer> result = htmlProcessor.processHtml(getHtmlFromResource("htmlTest.html"));
         Assert.assertEquals(59, result.size());
         Assert.assertTrue(result.containsKey("Здесь"));
         Assert.assertEquals(Integer.valueOf(1), result.get("нет"));
@@ -21,7 +23,7 @@ public class HtmlProcessorTest {
 
     private String getHtmlFromResource(String path) {
         StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(path), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
